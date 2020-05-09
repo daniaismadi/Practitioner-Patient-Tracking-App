3458d83f7bdecddf3575714d069d7ef7f45cca6d
daniaismadi <dania.ismadi@mail.utoronto.ca>
2020-05-09 15:35:51 +0800

    Updated Mongo file

--- src/main/java/database/Mongo.java
+++ src/main/java/database/Mongo.java
@@ -5,10 +5,11 @@ import com.mongodb.client.MongoDatabase;
 
 public class Mongo {
     static MongoDatabase db;
+    static final String DATABASE_NAME = "FIT3077JD";
 
     public static void connect() {
         MongoClient mongoClient = new MongoClient();
         System.out.println("Server connected!");
-        db = mongoClient.getDatabase("test");
+        db = mongoClient.getDatabase(DATABASE_NAME);
     }
 }
