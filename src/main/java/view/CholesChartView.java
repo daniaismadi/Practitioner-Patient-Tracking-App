dfa035b99a8c26d4c2c641c740d2bcc218eec777
Junaid Althaf <mjun0003@student.monash.edu>
2020-06-01 17:13:32 +0800

    Sample barchart for requirement 1
    Former-commit-id: 7b02a1676dc10ce54d2474c01432813ad129085d

---
+++ src/main/java/view/CholesChartView.java
@@ -0,0 +1,55 @@
+package view;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
+import javax.swing.*;
+
+public class CholesChartView extends JFrame {
+
+    private JPanel rootPanel;
+
+    public CholesChartView(){
+
+        JFreeChart barChart = chart();
+        ChartPanel chartPanel = new ChartPanel(barChart);
+        CategoryPlot categoryPlot = barChart.getCategoryPlot();
+        BarRenderer br = (BarRenderer) categoryPlot.getRenderer();
+        br.setMaximumBarWidth(.1); // set maximum width to 35% of chart
+        setContentPane(chartPanel);
+    }
+
+    public JFreeChart chart(){
+        JFreeChart barChart = ChartFactory.createBarChart("Total Cholestrol mg/dl", "","",createDataSet());
+        return barChart;
+    }
+
+
+    public CategoryDataset createDataSet(){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(7,"","Junaid");
+        dataset.addValue(10,"","Gaffar");
+        dataset.addValue(4,"","Junaid2");
+        dataset.addValue(30,"","Gaffar2");
+        dataset.addValue(4,"","Junaid3");
+        dataset.addValue(45,"","Gaffar3");
+
+        return dataset;
+    }
+
+    public static void main(String[] args) {
+        CholesChartView chart = new CholesChartView();
+        chart.pack();
+//        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
+
+    }
+}
